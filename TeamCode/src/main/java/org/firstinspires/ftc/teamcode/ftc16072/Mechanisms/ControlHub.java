c9f40c3b7ce8c8b0403479831ff43a332a2d1bc6
Alan Smith <alan@randomsmiths.com>
2024-05-20 20:59:10 -0400

    work on control hub mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/ControlHub.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/ControlHub.java
@@ -1,19 +1,40 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
 
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.ftc16072.Tests.QQTest;
+import org.firstinspires.ftc.teamcode.ftc16072.Tests.TestGyro;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ControlHub extends QQMechanism{
+    IMU gyro;
     @Override
     public void init(HardwareMap hwMap) {
+        gyro = hwMap.get(IMU.class,"imu");
 
-    }
+        final double xRotationDegrees = 0;
+        final double yRotationDegrees = 0;
+        final double zRotation = 0;
+
+        Orientation hubRotation =
+                RevHubOrientationOnRobot.xyzOrientation(xRotationDegrees,yRotationDegrees,zRotation);
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(hubRotation);
+
+        gyro.initialize()
+
+
+        }
 
     @Override
     public List<QQTest> getTests() {
-        return null;
+        return Collections.singletonList(new TestGyro("Gyro", gyro));
     }
+
 }
